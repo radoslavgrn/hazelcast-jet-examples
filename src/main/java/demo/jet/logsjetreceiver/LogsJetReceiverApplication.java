569030dc077ce00524498b5d9b389199d89b8e34
radoslavgrn <radoslavgrn@gmail.com>
2022-10-16 17:38:11 +0300

    init

---
+++ src/main/java/demo/jet/logsjetreceiver/LogsJetReceiverApplication.java
@@ -0,0 +1,49 @@
+package demo.jet.logsjetreceiver;
+
+import com.hazelcast.collection.IList;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import java.util.List;
+import org.apache.commons.lang3.ArrayUtils;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.ContentDisposition;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/logs")
+public class LogsJetReceiverApplication {
+
+  static final String FILE_LIST = "FILE-LIST";
+
+  public static void main(String[] args) {
+    SpringApplication.run(LogsJetReceiverApplication.class, args);
+  }
+
+  @GetMapping("/file")
+  public ResponseEntity<ByteArrayResource> getFile() {
+    HazelcastInstance hz = Hazelcast.newHazelcastInstance();
+
+    IList<Byte> array = hz.getList(FILE_LIST);
+
+    Byte[] bytes = array.toArray(new Byte[0]);
+
+    ByteArrayResource resource = new ByteArrayResource(ArrayUtils.toPrimitive(bytes));
+    return ResponseEntity.ok()
+        .contentType(MediaType.APPLICATION_OCTET_STREAM)
+        .contentLength(resource.contentLength())
+        .header(HttpHeaders.CONTENT_DISPOSITION,
+            ContentDisposition.attachment()
+                .filename("test")
+                .build().toString())
+        .body(resource);
+  }
+
+}
---
+++ src/test/java/demo/jet/logsjetreceiver/LogsJetReceiverApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.jet.logsjetreceiver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogsJetReceiverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
